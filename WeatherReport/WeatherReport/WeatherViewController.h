7846cc5ba88396e7bf0bf37bee77f6655601b209
Sveinn Arnorsson <sveinnoa@gmail.com>
2013-05-23 12:47:58 +0000

    remove submodule fuckup

---
+++ WeatherReport/WeatherReport/WeatherViewController.h
@@ -0,0 +1,12 @@
+//
+//  WeatherViewController.h
+//  WeatherReport
+//
+//  Created by Otti Hólm Elínarson on 5/15/13.
+//  Copyright (c) 2013 Otti Elínarson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WeatherViewController : UIViewController <NSURLConnectionDelegate, NSXMLParserDelegate>
+@end
