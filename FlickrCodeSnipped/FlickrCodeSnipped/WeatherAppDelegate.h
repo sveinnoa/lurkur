7c11bedadd396f7f1d4c897878d8d88f41289211
Sveinn Arnorsson <sveinnoa@gmail.com>
2013-05-23 11:35:53 +0000

    Refactoring

---
+++ FlickrCodeSnipped/FlickrCodeSnipped/WeatherAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  WeatherAppDelegate.h
+//  WeatherReport
+//
+//  Created by Otti Hólm Elínarson on 5/15/13.
+//  Copyright (c) 2013 Otti Elínarson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WeatherAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
