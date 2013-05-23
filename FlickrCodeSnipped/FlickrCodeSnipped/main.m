3a4cbf1fb6a54d85e2cf64025d6fdc6d5eab8ee3
Sveinn Arnorsson <sveinnoa@gmail.com>
2013-05-23 11:35:12 +0000

    Refactoring

--- FlickrCodeSnipped/FlickrCodeSnipped/main.m
+++ FlickrCodeSnipped/FlickrCodeSnipped/main.m
@@ -8,11 +8,11 @@
 
 #import <UIKit/UIKit.h>
 
-#import "FlickrCodeSnippedAppDelegate.h"
+#import "WeatherAppDelegate.h"
 
 int main(int argc, char *argv[])
 {
     @autoreleasepool {
-        return UIApplicationMain(argc, argv, nil, NSStringFromClass([FlickrCodeSnippedAppDelegate class]));
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([WeatherAppDelegate class]));
     }
 }
